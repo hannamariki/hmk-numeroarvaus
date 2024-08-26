4d3c714fdb8debc2948445454997973f7814fcbf
Hanna-Mari Kielenniva <h.kielenniva@gmail.com>
2024-08-26 20:58:00 +0300

    Tehtävä2 valmis

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'; 
+import {SafeAreaView, StyleSheet, TextInput, Button, View,Alert,Text} from 'react-native'; 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [tulos, setTulos] = useState('Guess a number between 1-100');
+  const [syote, setSyote] = useState('');
+  const [arvaus, setArvaus] = useState(0);
+  const [vastaus, setVastaus] = useState(Math.floor(Math.random() * 100) + 1)
+ 
+
+  const peli = () => { 
+    let arvaus1 = parseInt(syote)
+    if (arvaus1 < vastaus){
+      arvaus1 = 'Your guess ' + syote + ' is too low'
+      setArvaus(arvaus+1);
+      setTulos(arvaus1.toString());
+    }else if (arvaus1 > vastaus){
+       arvaus1 = 'Your guess ' + syote + ' is too high';
+       setArvaus(arvaus+1);
+      setTulos(arvaus1.toString());
+    }else{
+      Alert.alert('You guessed the number in ' + arvaus + ' guesses') //tällä saadaan erillinen ponnahduskenttä
+      setArvaus(0);
+      setVastaus((Math.floor(Math.random() * 100) + 1))
+    }
+  };
+
+
+  return ( 
+    <SafeAreaView style={styles.container}>   
+      <Text> 
+        {tulos}
+      </Text>
+      <TextInput style={styles.input}
+        value={syote}
+        onChangeText={text => setSyote(text)}
+        placeholder="   "
+        keyboardType="numeric"
+      />
+      <View>
+        <View> 
+          <Button
+            title="MAKE GUESS"
+            onPress={peli} 
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  ); 
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +57,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: { // määrittelee tekstikentän ulkoasun
+    height: 40,
+    margin: 10,
+    borderWidth: 1,
+    padding: 10,
+    textAlign: "center",
+  }
 });
